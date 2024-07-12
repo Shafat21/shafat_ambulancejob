72aa894faa5110f81311469e913fcaa87604ddd2
Shafat Alam <61838416+Shafat21@users.noreply.github.com>
2024-07-12 23:04:20 +0600

    Update appel.lua

--- shafat_ambulancejob/client/appel.lua
+++ shafat_ambulancejob/client/appel.lua
@@ -1,11 +1,5 @@
-
-ESX                             = nil
-
 Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
+	ESX = exports["es_extended"]:getSharedObject()
 
 	while ESX.GetPlayerData().job == nil do
 		Citizen.Wait(10)
